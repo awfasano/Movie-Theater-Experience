f013711915c404ec6ff4db03dcfc8cb97962ed90
awfasano <awfasano@gmail.com>
2025-08-13 14:38:44 -0400

    initial commits for world builder

---
+++ Movie Theater Experience/World Builder/PlacementGrid.swift
@@ -0,0 +1,8 @@
+//
+//  PlacementGrid.swift
+//  Movie Theater Experience
+//
+//  Created by Anthony Fasano on 8/13/25.
+//
+
+import Foundation
---
+++ Movie Theater Experience/World Builder/WorldData.swift
@@ -0,0 +1,8 @@
+//
+//  WorldData.swift
+//  Movie Theater Experience
+//
+//  Created by Anthony Fasano on 8/13/25.
+//
+
+import Foundation
