3f5b87e5574830d41de4efb82f3733962343e97e
awfasano <awfasano@gmail.com>
2025-09-24 14:36:14 -0400

    testing data

---
+++ Movie Theater Experience/Hosted Events/Views/FirebaseDebugView.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseDebugView.swift
+//  Movie Theater Experience
+//
+//  Created by Anthony Fasano on 9/23/25.
+//
+
+import Foundation
---
+++ Movie Theater Experience/Hosted Events/Views/FirebaseTestDataSetup.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseTestDataSetup.swift
+//  Movie Theater Experience
+//
+//  Created by Anthony Fasano on 9/23/25.
+//
+
+import Foundation
